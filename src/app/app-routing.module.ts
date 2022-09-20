eb7a96ca15a00f78b5e6130a73ef8f7f5befbaeb
Omar Huaman Barrial <omar16hb@hotmail.com>
2022-09-20 08:14:34 -0500

    Ventanas separadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConductorComponent } from './page/conductor/conductor.component';
+import { PadredefamiliaComponent } from './page/padredefamilia/padredefamilia.component';
+import { EstadoComponent } from './page/estado/estado.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'conductor', component:ConductorComponent, children: [
+    
+
+  ]
+}, {
+  path: 'padredefamilia', component: PadredefamiliaComponent, children: [
+
+  ]
+},
+{
+  path: 'estado', component:EstadoComponent, children: [
+
+  ]
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
