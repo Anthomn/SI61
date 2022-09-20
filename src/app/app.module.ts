8c6b658c2b72752a66afc793dde24433cac13691
Anedre <anedre12345@gmail.com>
2022-09-19 19:47:23 -0500

    componente estado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,9 @@ import { ConductorListarComponent } from './page/conductor/conductor-listar/cond
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import{MatTableModule} from '@angular/material/table';
 import { PadredefamiliaComponent } from './page/padredefamilia/padredefamilia.component';
-import { PadredefamiliaListarComponent } from './page/padredefamilia/padredefamilia-listar/padredefamilia-listar.component'
+import { PadredefamiliaListarComponent } from './page/padredefamilia/padredefamilia-listar/padredefamilia-listar.component';
+import { EstadoComponent } from './page/estado/estado.component';
+import { EstadoListarComponent } from './page/estado/estado-listar/estado-listar.component'
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { PadredefamiliaListarComponent } from './page/padredefamilia/padredefami
     ConductorComponent,
     ConductorListarComponent,
     PadredefamiliaComponent,
-    PadredefamiliaListarComponent
+    PadredefamiliaListarComponent,
+    EstadoComponent,
+    EstadoListarComponent
   ],
   imports: [
     BrowserModule,
