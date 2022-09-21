492b6e1838f4e1ecdfba6c7d6e5d736d85cd7dbb
Omar Huaman Barrial <omar16hb@hotmail.com>
2022-09-21 01:58:59 -0500

    service creaedita

--- src/app/service/padredefamilia.service.ts
+++ src/app/service/padredefamilia.service.ts
@@ -2,14 +2,24 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Padredefamilia } from '../model/padredefamilia';
 import { environment } from 'src/environments/environment';
-
+import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PadredefamiliaService {
   private url:string= "http://localhost:5003/padredefamilia"
+  private listaCambio = new Subject<Padredefamilia[]>()
   constructor(private http:HttpClient) { }
   listar(){
     return this.http.get<Padredefamilia[]>(this.url);
+  }
+  insertar(padredefamilia: Padredefamilia) {
+    return this.http.post(this.url, padredefamilia);
+  }
+  setLista(listaNueva: Padredefamilia[]) {
+    this.listaCambio.next(listaNueva);
+  }
+  getLista() {
+    return this.listaCambio.asObservable();
   }
 }
--- src/app/service/ubicacion.service.ts
+++ src/app/service/ubicacion.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Ubicacion } from '../model/ubicacion';
+import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UbicacionService {
 
   private url:string= "http://localhost:5003/ubicacion"
-
+  private listaCambio = new Subject<Ubicacion[]>()
   constructor(private http:HttpClient) { }
   listar(){
     return this.http.get<Ubicacion[]>(this.url);
+  }
+  insertar(ubicacion: Ubicacion) {
+    return this.http.post(this.url, ubicacion);
+  }
+  setLista(listaNueva: Ubicacion[]) {
+    this.listaCambio.next(listaNueva);
+  }
+  getLista() {
+    return this.listaCambio.asObservable();
   }
 }
